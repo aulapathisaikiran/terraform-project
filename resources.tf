d8a2457bde6555c585d37b705f5ddbfe8c3d2ed1
aulapathisaikiran <91209589+aulapathisaikiran@users.noreply.github.com>
2023-05-11 12:31:51 +0530

    Update resources.tf

--- resources.tf
+++ resources.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "mycloudserver" {
-  ami           = "ami-033b95fb8079dc481"
+  ami           = "ami-0c768662cc797cd75"
   instance_type = "t2.micro"
  tags = {
     Name = "mycloudserver"
